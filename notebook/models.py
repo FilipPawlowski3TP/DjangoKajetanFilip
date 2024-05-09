c7343f94816490649b34d9bb8b2d48f00565be1b
user <dupa@dupa>
2024-05-09 17:39:00 +0200

    komit dla łysych ludzi

--- notebook/models.py
+++ notebook/models.py
@@ -1,4 +1,8 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.urls import reverse
+from django.utils import timezone
+
 
 # Create your models here.
 class Notebook(models.Model):
@@ -6,6 +10,7 @@ class Notebook(models.Model):
     body = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    publish = models.DateTimeField(default=timezone.now)
 
     PRIORITY_CHOICES = (
         ("low", "Niski"),
@@ -13,4 +18,22 @@ class Notebook(models.Model):
         ("high", "Wysoki")
     )
 
+    STATUS_CHOICES = (
+        ('draft', 'Draft'),
+        ('published', 'Opublikowany')
+    )
+
     priority = models.CharField(max_length=10, choices=PRIORITY_CHOICES, default="low")
+    slug = models.SlugField(max_length=200, unique_for_date='publish')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='notebooks')
+
+    class Meta:
+        ordering = ('high','-publish')
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('djangoProject:notes_detail', args=[self.publish.year,
+                                                 self.publish.month, self.publish.day,
+                                                 self.slug])
--- notebook/views.py
+++ notebook/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views import View
+
+from notebook.models import Notebook
+
 
 # Create your views here.
+class notesView(View):
+    queryset = Notebook.published.all()
+    context_object_name = "notes"
+    paginate_by = 6
+    template_name = "notes/note/list.html"
+
+def notes_detail(request, year, month, day, slug):
+    note = get_object_or_404(Notebook, slug=slug, status='published',
+                             published_date__year=year, published_date__month=month,
+                             published_date__day=day)
+    return render(request, 'notes/note/detail.html', {'note': note})
